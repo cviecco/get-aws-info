4e78f6b8d394dbc8297a4ffe229323fd3b9bc56d
camilo_viecco1 <camilo_viecco1@symantec.com>
2017-02-02 01:01:08 +0000

    now with actual funcionality

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func getInstanceId() (string, error) {
 	return identityDocument.InstanceID, nil
 }
 
-func getTag(name string, regionString string) (string, error) {
+func getTag(requestedTagName string, regionString string) (string, error) {
 	//svc := ec2.New(mySession, aws.NewConfig().WithRegion("us-west-2"))
 	instanceID, err := getInstanceId()
 	if err != nil {
@@ -37,42 +37,6 @@ func getTag(name string, regionString string) (string, error) {
 	}
 
 	svc := ec2.New(session.New(&aws.Config{Region: aws.String(regionString)}))
-
-	//svc := ec2.New(sess)
-
-	params := &ec2.DescribeInstancesInput{
-		//DryRun: aws.Bool(true),
-		Filters: []*ec2.Filter{
-			{ /*
-				// Required
-				Name: aws.String("String"),
-				Values: []*string{
-					aws.String("String"), // Required
-					// More values...
-				},
-			*/
-			},
-			// More values...
-		},
-		InstanceIds: []*string{
-			//aws.String("i-0c8b43c46be4852d5"), // Required
-			aws.String(instanceID),
-			// More values...
-		},
-		//MaxResults: aws.Int64(1),
-		//NextToken: aws.String("String"),
-	}
-	resp, err := svc.DescribeInstances(params)
-
-	if err != nil {
-		// Print the error, cast err to awserr.Error to get the Code and
-		// Message from an error.
-		fmt.Println(err.Error())
-		return "", err
-	}
-
-	// Pretty-print the response data.
-	fmt.Println(resp)
 
 	describeTagsparams := &ec2.DescribeTagsInput{
 		//DryRun: aws.Bool(true),
@@ -107,7 +71,15 @@ func getTag(name string, regionString string) (string, error) {
 	}
 
 	// Pretty-print the response data.
-	fmt.Println(resp2)
+	if debug {
+		fmt.Println(resp2)
+	}
+	for _, tag := range resp2.Tags {
+		if *tag.Key == requestedTagName {
+			log.Printf("found %s: %s\n", *tag.Key, *tag.Value)
+			return *tag.Value, nil
+		}
+	}
 
 	return "", nil
 }
@@ -121,6 +93,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("Cannot get Region string %s", err)
 	}
-	getTag(*tagName, regionString)
+	value, err := getTag(*tagName, regionString)
+	if err != nil {
+		log.Fatalf("Cannot get Tag Name %s", err)
+	}
+	fmt.Printf("%s\n", value)
 
 }
