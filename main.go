3952b424b5df639646407f402fe1b1cf60bf7e04
camilo_viecco1 <camilo_viecco1@symantec.com>
2017-02-02 00:39:22 +0000

    now via describetags

--- main.go
+++ main.go
@@ -74,6 +74,41 @@ func getTag(name string, regionString string) (string, error) {
 	// Pretty-print the response data.
 	fmt.Println(resp)
 
+	describeTagsparams := &ec2.DescribeTagsInput{
+		//DryRun: aws.Bool(true),
+		Filters: []*ec2.Filter{
+			{ // Required
+				Name: aws.String("resource-type"),
+				Values: []*string{
+					aws.String("instance"), // Required
+					// More values...
+				},
+			},
+			{
+				Name: aws.String("resource-id"),
+				Values: []*string{
+					aws.String(instanceID), // Required
+					// More values...
+				},
+			},
+			// More values...
+		},
+		MaxResults: aws.Int64(10),
+		NextToken:  aws.String("String"),
+	}
+
+	resp2, err := svc.DescribeTags(describeTagsparams)
+
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	// Pretty-print the response data.
+	fmt.Println(resp2)
+
 	return "", nil
 }
 
